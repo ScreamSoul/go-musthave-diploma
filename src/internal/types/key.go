d51fbb135aa41118dff4d962a3dff6de09209203
alex <alex@mathun.io>
2024-05-28 19:10:06 +0300

    Fix

--- src/internal/types/key.go
+++
@@ -1,9 +0,0 @@
-package types
-
-// Key представляет собой пользовательский тип ключа для контекста.
-type Key int
-
-const (
-	// UserID - ключ для доступа к user_id в контексте
-	UserID Key = iota
-)
